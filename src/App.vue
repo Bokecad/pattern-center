7d5c8b33d212aed56443c0fb3414e6e7b21b1102
maqingzhuan <xuanxing1@163.com>
2017-09-07 17:02:35 +0800

    add App

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <template>
   <div id="app">
     <router-view></router-view>
-    <personal-header></personal-header>
   </div>
 </template>
 
 <script>
 //var imgUrl = require('@/assets/logo.png')
-import PersonalHeader from '@/components/PersonalHeader'
 
 
 export default {
@@ -15,23 +13,16 @@ export default {
   data(){
     return {
     }
-  },
-  components:{
-    PersonalHeader
   }
 }
 </script>
 
 <style lang="less">
 
-@import url('pages/style/common.less');
 
 
 
 #app {
-  .wrap1{width:1280px;}
-  .wrap2{width:1200px;}
-  background: @bg-body;
 }
 
 </style>
